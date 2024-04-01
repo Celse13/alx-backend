3ff8d54f11ae8e740d63db679f885004e90a8613
Celse13 <117368635+Celse13@users.noreply.github.com>
2024-04-01 18:43:06 +0200

    Update 1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -7,4 +7,4 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """ Pagination by the index"""
     starting_index = (page - 1) * page_size
     ending_index = page * page_size
-    return starting_index, ending_index
+    return (starting_index, ending_index)
