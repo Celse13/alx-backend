c46315c4d00f3c2154c75274fcebac78e7e0edbb
Celse13 <117368635+Celse13@users.noreply.github.com>
2024-04-01 19:48:13 +0200

    Update 1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Implementation of simple pagination"""
-from typing import Tuple
+from typing import Tuple, List
+import csv
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
@@ -8,3 +9,29 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     starting_index = (page - 1) * page_size
     ending_index = page * page_size
     return (starting_index, ending_index)
+
+
+class Server:
+    """ This class will aid in pagination of the server"""
+    CSV_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self) -> None:
+        """ Constructor for the class"""
+        self.__dataset = None
+
+    def dataset(self) -> list[list]:
+        """ Storing pagination data"""
+        if self.dataset is None:
+            with open(self.CSV_FILE) as file:
+                read_from_file = csv.reader(file)
+                dataset = [row for row in read_from_file]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ Getting the page"""
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+        starting_page, ending_page = index_range(page, page_size)
+        return self.dataset()[starting_page:ending_page]
