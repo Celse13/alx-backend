fbd26f136d4385528174bd800b75a74681cdae0a
Celse13 <117368635+Celse13@users.noreply.github.com>
2024-04-01 19:57:03 +0200

    Update 1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
-""" Implementation of simple pagination"""
-from typing import Tuple, List
+""" Implementation of simple pagination """
 import csv
 import math
+from typing import List, Tuple
 
 
 class Server:
     """ This class will aid in pagination of the server"""
+
     CSV_FILE = "Popular_Baby_Names.csv"
 
-    def __init__(self) -> None:
+    def __init__(self):
         """ Constructor for the class"""
         self.__dataset = None
 
-    def dataset(self) -> list[list]:
+    def dataset(self) -> List[List]:
         """ Storing pagination data"""
-        if self.dataset is None:
+        if self.__dataset is None:
             with open(self.CSV_FILE) as file:
                 read_from_file = csv.reader(file)
                 dataset = [row for row in read_from_file]
@@ -24,7 +25,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """ Getting the page"""
+        """ Getting the page """
         assert isinstance(page, int) and isinstance(page_size, int)
         assert page > 0 and page_size > 0
         starting_page, ending_page = self.index_range(page, page_size)
