e6f31dd3b51b90d2c2f5714793b1d8bdd22f0d15
Celse13 <117368635+Celse13@users.noreply.github.com>
2024-04-01 18:54:39 +0200

    Update 1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
-"""Pagination helper function.
-"""
+""" Implementation of simple pagination"""
 from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """Retrieves the index range from a given page and page size.
-    """
-
-    return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
+    """ Pagination by the index"""
+    starting_index = (page - 1) * page_size
+    ending_index = page * page_size
+    return (starting_index, ending_index)
