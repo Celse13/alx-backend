bbb2e393a07c4f52780f71f1f2817121367d8f69
Celse13 <117368635+Celse13@users.noreply.github.com>
2024-04-03 22:37:13 +0200

    Update 4-mru_cache.py

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,31 +1,39 @@
 #!/usr/bin/python3
-""" Caching System """
+""" caching system """
 BaseCaching = __import__("base_caching").BaseCaching
 
 
-class MRUCache(BaseCaching):
-    """Constructor"""
+class LFUCache(BaseCaching):
+    """caching system"""
 
     def __init__(self):
+        """Initialize"""
         super().__init__()
-        self.keys = []
+        self.frequency = {}
 
-    def put(self, key, item):
-        """Add an item in cache"""
-        if key is not None and item is not None:
-            if key in self.cache_data:
-                self.keys.remove(key)
-            elif len(self.keys) >= self.MAX_ITEMS:
-                discarded = self.keys.pop()
-                del self.cache_data[discarded]
-                print("DISCARD: {}".format(discarded))
-            self.cache_data[key] = item
-            self.keys.append(key)
+    def store(self, key, value):
+        """Store an item in the cache"""
+        if key is None or value is None:
+            return
 
-    def get(self, key):
-        """Get an item by key from the cache"""
-        if key is not None and key in self.cache_data:
-            self.keys.remove(key)
-            self.keys.append(key)
+        if key in self.cache_data:
+            self.cache_data[key] = value
+            self.frequency[key] += 1
+        else:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                min_freq = min(self.frequency.values())
+                least_freq_keys = [
+                    k for k, v in self.frequency.items() if v == min_freq]
+                lfu_key = min(least_freq_keys, key=self.frequency.get)
+                self.cache_data.pop(lfu_key)
+                self.frequency.pop(lfu_key)
+                print("REMOVED:", lfu_key)
+
+            self.cache_data[key] = value
+            self.frequency[key] = 1
+
+    def retrieve(self, key):
+        """Retrieve an item from the cache"""
+        if key in self.cache_data:
+            self.frequency[key] += 1
             return self.cache_data.get(key)
-        return None
