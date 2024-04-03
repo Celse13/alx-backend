b1081b939d6b48602b3b0fe5c41317a22079b5b2
Celse13 <117368635+Celse13@users.noreply.github.com>
2024-04-03 22:37:45 +0200

    Update 4-mru_cache.py

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,39 +1,40 @@
 #!/usr/bin/python3
-""" caching system """
+""" doc doc doc """
 BaseCaching = __import__("base_caching").BaseCaching
 
 
 class LFUCache(BaseCaching):
-    """caching system"""
+    """doc doc doc"""
 
     def __init__(self):
-        """Initialize"""
+        """doc doc doc"""
         super().__init__()
-        self.frequency = {}
+        self.freq = {}
 
-    def store(self, key, value):
-        """Store an item in the cache"""
-        if key is None or value is None:
+    def put(self, key, item):
+        """doc doc doc"""
+        if key is None or item is None:
             return
 
         if key in self.cache_data:
-            self.cache_data[key] = value
-            self.frequency[key] += 1
+            self.cache_data[key] = item
+            self.freq[key] += 1
         else:
             if len(self.cache_data) >= self.MAX_ITEMS:
-                min_freq = min(self.frequency.values())
+                min_freq = min(self.freq.values())
                 least_freq_keys = [
-                    k for k, v in self.frequency.items() if v == min_freq]
-                lfu_key = min(least_freq_keys, key=self.frequency.get)
+                    k for k, v in self.freq.items() if v == min_freq
+                ]
+                lfu_key = min(least_freq_keys, key=self.freq.get)
                 self.cache_data.pop(lfu_key)
-                self.frequency.pop(lfu_key)
-                print("REMOVED:", lfu_key)
+                self.freq.pop(lfu_key)
+                print("DISCARD:", lfu_key)
 
-            self.cache_data[key] = value
-            self.frequency[key] = 1
+            self.cache_data[key] = item
+            self.freq[key] = 1
 
-    def retrieve(self, key):
-        """Retrieve an item from the cache"""
+    def get(self, key):
+        """doc doc doc"""
         if key in self.cache_data:
-            self.frequency[key] += 1
+            self.freq[key] += 1
             return self.cache_data.get(key)
