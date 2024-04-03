1e3417d1996ee910836bdd8594fe0ae4a6b20737
Celse13 <117368635+Celse13@users.noreply.github.com>
2024-04-03 17:49:12 +0200

    Create 0-basic_cache.py

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Caching """
+
+
+class BaseCaching:
+    """ BaseCaching class """
+
+    def __init__(self):
+        """ Constructor """
+        self.cache_data = {}
+
+
+class BaseCache(BaseCaching):
+    """ BaseCache class """
+
+    def put(self, key, item):
+        """ add an item in a cache"""
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
