66279bff757035d1b5df7890493caa047ca9d1d4
Celse13 <117368635+Celse13@users.noreply.github.com>
2024-04-03 22:03:08 +0200

    Update 4-mru_cache.py

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -4,24 +4,28 @@ BaseCaching = __import__("base_caching").BaseCaching
 
 
 class MRUCache(BaseCaching):
+    """Constructor"""
+
     def __init__(self):
         super().__init__()
-        self.mru_key = []
+        self.keys = []
 
     def put(self, key, item):
-        """ Put an item in cache"""
+        """Add an item in cache"""
         if key is not None and item is not None:
             if key in self.cache_data:
-                del self.cache_data[key]
-            elif len(self.cache_data) >= self.MAX_ITEMS:
-                del self.cache_data[self.mru_key]
-                print(f"DISCARD: {self.mru_key}")
+                self.keys.remove(key)
+            elif len(self.keys) >= self.MAX_ITEMS:
+                discarded = self.keys.pop(0)
+                del self.cache_data[discarded]
+                print("DISCARD: {}".format(discarded))
             self.cache_data[key] = item
-            self.mru_key = key
+            self.keys.append(key)
 
     def get(self, key):
-        """ Get an item by key from cache"""
-        if key is None or key not in self.cache_data:
-            return None
-        self.mru_key = key
-        return self.cache_data[key]
+        """Get an item by key from the cache"""
+        if key is not None and key in self.cache_data:
+            self.keys.remove(key)
+            self.keys.append(key)
+            return self.cache_data.get(key)
+        return None
