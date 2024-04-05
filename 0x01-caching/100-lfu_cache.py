042f6b9dfc8d916628efffe7a3e815297b3e1449
Celse13 <117368635+Celse13@users.noreply.github.com>
2024-04-05 22:03:43 +0200

    Update 100-lfu_cache.py

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,73 +1,65 @@
 #!/usr/bin/env python3
-"""5. LFU Caching
-"""
+""" LFU Caching module """
 from collections import OrderedDict
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LFUCache(BaseCaching):
-    """Least Frequently Used Cache Method
-    """
+    """ LFU Caching class"""
     def __init__(self):
-        """init method
-        """
+        """init method"""
         super().__init__()
         self.cache_data = OrderedDict()
-        self.usage_counts = {}
-        self.least_freq_used = None
+        self.counts = {}
+        self.least_count = None
 
-    def put(self, key, item):
-        """adds cache to cache data
-        """
-        if key is None or item is None:
+    def put(self, key, value):
+        """adds key value pair to the cache"""
+        if key is None or value is None:
             return
 
         if key in self.cache_data:
-            self.cache_data[key] = item
-            self.usage_counts[key] += 1
+            self.cache_data[key] = value
+            self.counts[key] += 1
         else:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                self.evict()
-            self.cache_data[key] = item
-            self.usage_counts[key] = 1
+                self.remove_least_used()
+            self.cache_data[key] = value
+            self.counts[key] = 1
 
-        self.update_least_freq_used()
+        self.update_least_used()
 
     def get(self, key):
-        """retrieves a cache data
-        """
+        """gets the value of key from cache"""
         if key is None or key not in self.cache_data:
             return None
-        self.usage_counts[key] += 1
-        self.update_least_freq_used()
+        self.counts[key] += 1
+        self.update_least_used()
         return self.cache_data[key]
 
-    def evict(self):
-        """check for cache data used less frequently
-        """
+    def remove_least_used(self):
+        """evicts the least frequently used item from cache"""
         if not self.cache_data:
             return
 
-        least_used = min(self.usage_counts.values())
-        candidates = [k for k, v in self.usage_counts.items()
-                      if v == least_used]
+        least_used_count = min(self.counts.values())
+        least_used_keys = [k for k, v in self.counts.items()
+                           if v == least_used_count]
 
-        if len(candidates) > 1:
-            # Apply LRU policy among the candidates
-            for key in self.cache_data:
-                if key in candidates:
-                    evict_key = key
+        if len(least_used_keys) > 1:
+            for k in self.cache_data:
+                if k in least_used_keys:
+                    least_used_key = k
                     break
         else:
-            evict_key = candidates[0]
+            least_used_key = least_used_keys[0]
 
-        print(f'DISCARD: {evict_key}')
-        del self.cache_data[evict_key]
-        del self.usage_counts[evict_key]
+        print(f'DISCARD: {least_used_key}')
+        del self.cache_data[least_used_key]
+        del self.counts[least_used_key]
 
-    def update_least_freq_used(self):
-        """updates the number of frequency used
-        """
-        least_used = min(self.usage_counts.values())
-        self.least_freq_used = [k for k, v in self.usage_counts.items()
-                                if v == least_used]
+    def update_least_used(self):
+        """updates the least frequently used item in cache"""
+        least_used_count = min(self.counts.values())
+        self.least_count = [k for k, v in self.counts.items()
+                            if v == least_used_count]
